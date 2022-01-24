68047affb3aa611e2b3c645cbde547a1dfcd7629
fedjaz <fedjaz5@gmail.com>
2022-01-24 22:45:09 +0300

    add: login window

---
+++ FedjazContest/FedjazContest/Components/AccountViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using FedjazContest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FedjazContest.Components
+{
+    public class AccountViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            AccountComponentModel accountComponentModel;
+            if (true)
+            {
+                accountComponentModel = new AccountComponentModel(false, "/");
+            }
+            else
+            {
+                accountComponentModel = null;
+            }
+
+            return View(accountComponentModel);
+        }
+    }
+}
---
+++ FedjazContest/FedjazContest/Models/AccountComponentModel.cs
@@ -0,0 +1,15 @@
+﻿namespace FedjazContest.Models
+{
+    public class AccountComponentModel
+    {
+        public bool IsLogged { get; set; } = false;
+        public string CallbackUrl { get; set; } = "/";
+        //public User User { get; set; }
+
+        public AccountComponentModel(bool isLogged, string callbackUrl)
+        {
+            IsLogged = isLogged;
+            CallbackUrl = callbackUrl;
+        }
+    }
+}
